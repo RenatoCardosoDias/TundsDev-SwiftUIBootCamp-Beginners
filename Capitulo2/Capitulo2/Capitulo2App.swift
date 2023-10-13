5ed9770d560fb957adf10a14e65746eaa846676c
Renato Dias <renatocardosodias@icloud.com>
2023-10-13 12:16:33 -0300

    Criando um projeto para mostrar o funcionamento da propriedade State, value type e reference type

---
+++ Capitulo2/Capitulo2/Capitulo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Capitulo2App.swift
+//  Capitulo2
+//
+//  Created by Renato on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Capitulo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capitulo2/Capitulo2/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Capitulo2
+//
+//  Created by Renato on 13/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+	@State private var isOpen = false
+
+	var body: some View {
+		VStack {
+			Image(systemName: "arrow.down")
+				.rotationEffect(.init(degrees: isOpen ? 0 : 180))
+
+			Button {
+				withAnimation{
+					isOpen.toggle()
+				}
+			} label: {
+				Text("Toggle Arrow")
+			}
+		}
+	}
+}
+
+
+#Preview {
+	ContentView()
+}
