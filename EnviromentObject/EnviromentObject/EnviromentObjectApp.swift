ada2a0c7938b799e4f3d00b9fb1274c1af998da8
Renato Dias <renatocardosodias@icloud.com>
2023-10-17 19:12:11 -0300

    Criando um projeto fake para simular o pagamento de melhoria usando o EnviromentObject, caso o usuario tenha pago ele terá disponível várias outras funções

---
+++ EnviromentObject/EnviromentObject/EnviromentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnviromentObjectApp.swift
+//  EnviromentObject
+//
+//  Created by Renato on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct EnviromentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
