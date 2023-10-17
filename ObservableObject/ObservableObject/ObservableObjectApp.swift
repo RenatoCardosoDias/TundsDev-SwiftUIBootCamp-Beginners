8e66f82d2716d1c897bde27e1c2a976e7dd1d3ce
Renato Dias <renatocardosodias@icloud.com>
2023-10-17 18:15:16 -0300

    Projeto Observable Object and Published

---
+++ ObservableObject/ObservableObject/ObservableObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableObjectApp.swift
+//  ObservableObject
+//
+//  Created by Renato on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ObservableObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
