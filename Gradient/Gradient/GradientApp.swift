27ce5201fff54d2c09b0cd2dc54cd1eed0af0190
Renato Dias <renatocardosodias@icloud.com>
2023-10-17 13:27:19 -0300

    Usando Gradient em Swiftui, lienar, radial e angular

---
+++ Gradient/Gradient/GradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientApp.swift
+//  Gradient
+//
+//  Created by Renato on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
