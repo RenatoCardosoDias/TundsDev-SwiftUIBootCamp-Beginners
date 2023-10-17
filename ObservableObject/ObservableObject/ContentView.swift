43c8d01097070e855c561172c42798e4ceda5330
Renato Dias <renatocardosodias@icloud.com>
2023-10-17 18:23:45 -0300

    Criado dois States, um array de pessoas vazio e outro boleando, adiciondo um VStack para testar se a o State é verdadeiro, carregado um progressView para mostrar que está acontecendo algo, casso o array de pessoas estaja vazio informa uma mensagem, criado uma extension da ContentView com uma função de buscar pessoas, a função inicaliza com o State isLoading como verdade, carrega um array de nomes e finaliza com o State isLoading false

--- ObservableObject/ObservableObject/ContentView.swift
+++ ObservableObject/ObservableObject/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+
+	@State private var people: [String] = []
+	@State private var isLoading: Bool = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+			if isLoading {
+				ProgressView()
+				Text("Loading...")
+					.padding()
+			} else {
+				Text(people.isEmpty  ? "No Users" : "Managed to get \(people.count) users.")
+			}
+			Button(action: fetchPeople) {
+				Text("Fetch some people")
+			}
         }
         .padding()
     }
@@ -21,4 +31,16 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+//MARK: - EXTENSION
+
+private extension ContentView {
+	func fetchPeople () {
+		isLoading = true
+		DispatchQueue.main.asyncAfter(deadline: .now() + 1.5 ) {
+			people = ["Renato", "Sofia", "Erica"]
+			isLoading = false
+		}
+	}
 }
