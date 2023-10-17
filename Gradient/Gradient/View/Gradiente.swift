b70c2a262e2fdcd8d4fd318aba685be737d3c090
Renato Dias <renatocardosodias@icloud.com>
2023-10-17 15:39:12 -0300

    Alterando a posição do final do verde para deixar mais gradual

--- Gradient/Gradient/View/Gradiente.swift
+++ Gradient/Gradient/View/Gradiente.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct Gradiente: View {
     var body: some View {
 		ZStack{
-			LinearGradient(stops: ([.init(color: .black, location: 0.2), 					.init(color: .mint, location: 0.2)]),
+			LinearGradient(stops: ([.init(color: .black, location: 0.2), 					.init(color: .mint, location: 0.5)]),
 						   startPoint: .top, endPoint: .bottom)
 			.ignoresSafeArea()
 			Text("Sofia")
