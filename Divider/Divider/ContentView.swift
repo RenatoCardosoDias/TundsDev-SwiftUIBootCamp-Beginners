bf22ff6defb5e382f8ce94d22405c356d1faac8f
Renato Dias <renatocardosodias@icloud.com>
2023-10-19 17:17:47 -0300

    trabalhando com Divider

--- Divider/Divider/ContentView.swift
+++ Divider/Divider/ContentView.swift
@@ -10,10 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+			VStack {
+				Text("1. Divider Example Below")
+				Divider()
+					.frame(width:100)
+
+			}
+			.padding()
+			.background(.yellow)
+
+			Group {
+				Text("2.(Group) Example Below")
+				Divider()
+			}
+			.background(.mint)
+
+			HStack {
+				Text("1.(HStack) Example to the right")
+				Divider()
+					.background(.black)
+					.frame(height:50)
+				Image(systemName: "lock")
+			}
         }
         .padding()
     }
